73251ede7b273695f47ba49c1fd195061d74b683
Bhavya20252025 <learnifybhavya2025@gmail.com>
2026-05-17 02:19:16 +0530

    Create modules_ec2_instance main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = var.region 
+}
+resource "aws_instance" "ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.name
+  }
+}
+
+variable "region" {
+  
+}
+variable "ami" {
+  
+}
+variable "instance_type" {
+  
+}
+variable "name" {
+  
+}
